a7e093af12e39f7d6e0e4098381d938573087458
cbijan <cbijan2014@gmail.com>
2022-08-10 22:58:38 -0700

    Updated league ID to 2022 DieNasty

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "653857136408879104"; // your league ID
+export const leagueID = "798793968564416512"; // your league ID
 export const leagueName = "DieNa$ty"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
